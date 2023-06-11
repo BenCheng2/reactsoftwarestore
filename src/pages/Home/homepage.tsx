996b1edf3b2c1b5884eb812e12588619628263e0
BenCheng2 <bencheng0609@gmail.com>
2023-06-11 13:20:02 -0400

    feat(Pages): Setting up the grid in homepage

--- src/pages/Home/homepage.tsx
+++ src/pages/Home/homepage.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Product from "../../components/Product";
+import {Grid} from "@mui/material";
+
+const products = [
+    {
+        title: 'Clion',
+        subheader: 'IDE',
+        iconAddress: './pycharm.png',
+        product: 'Clion',
+        firstYearPrice: '$200',
+        secondYearPrice: '$400',
+        thirdYearPrice: '$600',
+        learnMore: '',
+        getQuote: '',
+        buyLink: '',
+    },
+    {
+        title: 'Jetbrain',
+        subheader: 'IDE',
+        iconAddress: './pycharm.png',
+        product: 'Clion',
+        firstYearPrice: '$200',
+        secondYearPrice: '$400',
+        thirdYearPrice: '$600',
+        learnMore: '',
+        getQuote: '',
+        buyLink: '',
+    },
+];
 
 const Homepage = () => {
     return (
         <div>
-            <Product />
+            <Grid container spacing={2} sx={{m:2}}>
+                {products.map((product) => {
+                    return (
+                        <Product key={product.title} {...product}/>
+                    )
+                })}
+
+            </Grid>
         </div>
     )
 }
